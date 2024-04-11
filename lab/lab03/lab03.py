fcb2cb749f4c4e78e3079b716d715d5dfaef53d1
cj8086 <yuyang0231@gmail.com>
2024-04-11 22:48:34 +0800

    submit: lab03

--- lab/lab03/lab03.py
+++ lab/lab03/lab03.py
@@ -14,7 +14,8 @@ def print_if(s, f):
     None
     """
     for x in s:
-        "*** YOUR CODE HERE ***"
+        if f(x) is True:
+            print(x)
 
 
 def close(s, k):
@@ -32,7 +33,8 @@ def close(s, k):
     """
     count = 0
     for i in range(len(s)):  # Use a range to loop over indices
-        "*** YOUR CODE HERE ***"
+        if abs(i - s[i]) <= k:
+            count += 1
     return count
 
 
@@ -47,7 +49,7 @@ def close_list(s, k):
     >>> close_list(t, 2)  # 2, 3, 4, and 5 are all within 2 of their index
     [2, 4, 3, 5]
     """
-    return [___ for i in range(len(s)) if ___]
+    return [s[i] for i in range(len(s)) if abs(i - s[i]) <= k]
 
 
 from math import sqrt
@@ -63,7 +65,7 @@ def squares(s):
     >>> squares(seq)
     []
     """
-    return [___ for n in s if ___]
+    return [round(sqrt(n)) for n in s if sqrt(n) == round(sqrt(n))]
 
 
 def double_eights(n):
@@ -87,7 +89,12 @@ def double_eights(n):
     >>> check(LAB_SOURCE_FILE, 'double_eights', ['While', 'For'])
     True
     """
-    "*** YOUR CODE HERE ***"
+    if n == 0:
+        return False 
+    if n % 100 == 88:
+        return True
+    else:
+        return double_eights(n // 10)
 
 
 def make_onion(f, g):
@@ -116,10 +123,10 @@ def make_onion(f, g):
     """
     def can_reach(x, y, limit):
         if limit < 0:
-            return ____
+            return False
         elif x == y:
-            return ____
+            return True
         else:
-            return can_reach(____, ____, limit - 1) or can_reach(____, ____, limit - 1)
+            return can_reach(f(x), y, limit - 1) or can_reach(g(x), y, limit - 1)
     return can_reach
 
