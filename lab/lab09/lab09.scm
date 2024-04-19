3596092fc50baa9f094511c91f0f6efb98b121d1
cj8086 <yuyang0231@gmail.com>
2024-04-19 21:59:12 +0800

    submit: lab09

--- lab/lab09/lab09.scm
+++ lab/lab09/lab09.scm
@@ -1,10 +1,21 @@
-(define (over-or-under num1 num2) 'YOUR-CODE-HERE)
+(define (over-or-under num1 num2)
+  (cond 
+    ((< num1 num2) -1)
+    ((= num1 num2) 0)
+    (else 1)))
 
-(define (make-adder num) 'YOUR-CODE-HERE)
+(define (make-adder num) 
+  (define (adder inc) (+ num inc))
+  adder)
 
-(define (composed f g) 'YOUR-CODE-HERE)
+(define (composed f g) 
+  (lambda (x)
+  (f (g x))))
 
-(define (repeat f n) 'YOUR-CODE-HERE)
+(define (repeat f n)
+  (if (= n 0)
+    (lambda (x) x)
+    (lambda (x) (f ((repeat f (- n 1)) x)))))
 
 (define (max a b)
   (if (> a b)
@@ -16,4 +27,11 @@
       b
       a))
 
-(define (gcd a b) 'YOUR-CODE-HERE)
+(define (gcd a b) 
+  (if (= a 0)
+    b
+    (if (= b 0)
+      a
+      (let ((x (max a b))
+            (y (min a b)))
+            (gcd y (modulo x y))))))
