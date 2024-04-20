c6c6ee7a94753b6f950ab3c3f8743b3c283758f5
cj8086 <yuyang0231@gmail.com>
2024-04-20 20:16:33 +0800

    submit: hw07

--- hw/hw07/hw07.scm
+++ hw/hw07/hw07.scm
@@ -1,15 +1,23 @@
 (define (square n) (* n n))
 
-(define (pow base exp) 'YOUR-CODE-HERE)
+(define (pow base exp)
+  (cond
+    ((zero? exp) 1)
+    ((even? exp) (square (pow base (floor (/ exp 2)))))
+    ((odd? exp) (* base (square (pow base (floor (/ exp 2))))))))
 
 (define (repeatedly-cube n x)
   (if (zero? n)
       x
-      (let (_________________)
+      (let (
+        (y (repeatedly-cube (- n 1) x))
+        )
         (* y y y))))
 
 (define (cddr s) (cdr (cdr s)))
 
-(define (cadr s) 'YOUR-CODE-HERE)
+; Returns the 2nd element of list s.
+(define (cadr s) (car (cdr s)))
 
-(define (caddr s) 'YOUR-CODE-HERE)
+; Returns the 3rd element of list s.
+(define (caddr s) (car (cddr s)))
