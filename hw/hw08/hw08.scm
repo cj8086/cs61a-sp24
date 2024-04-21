49f2ee10ea98fc4a88eb51ed23443bdbfb6c1a62
cj8086 <yuyang0231@gmail.com>
2024-04-21 14:46:43 +0800

    submit: hw08

--- hw/hw08/hw08.scm
+++ hw/hw08/hw08.scm
@@ -1,7 +1,33 @@
-(define (ascending? s) 'YOUR-CODE-HERE)
+(define (ascending? s) 
+  (if (<= (length s) 1)
+      #t
+      (let ((first (car s))
+             (second (car (cdr s))))
+        (and (<= first second)
+             (ascending? (cdr s))))))
 
-(define (my-filter pred s) 'YOUR-CODE-HERE)
+(define (my-filter pred s) 
+    (if (null? s)
+        '()
+        (if (pred (car s))
+            (cons (car s) (my-filter pred (cdr s)))
+            (my-filter pred (cdr s)))))
 
-(define (interleave lst1 lst2) 'YOUR-CODE-HERE)
+(define (interleave lst1 lst2)
+  (if (or (null? lst1) (null? lst2))
+      (append lst1 lst2)
+      (cons (car lst1) (cons (car lst2) (interleave (cdr lst1) (cdr lst2))))))
 
-(define (no-repeats s) 'YOUR-CODE-HERE)
+
+(define (no-repeats s)
+  (define (not-equal-to x)
+    (lambda (y) (not (= x y))))
+
+  (define (filter-non-repeats lst)
+    (if (null? lst)
+        '()
+        (cons (car lst)
+              (filter-non-repeats (filter (not-equal-to (car lst)) (cdr lst))))))
+
+  (filter-non-repeats s))
+
